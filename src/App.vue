887614b1c0d549d669d69f67abd292b013784d8d
wangcheng <gates@mucang.cn>
2019-08-30 16:37:47 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,92 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container" id="app">
+    <div class="row">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <span class="navbar-brand">NoteBook</span>
+        <router-link to="/" class="nav-item active nav-link">首页</router-link>
+        <a
+          v-if="!data.currentUser || !data.currentUser.name"
+          class="my-2 my-lg-0"
+          href="/login/oauth/github/auth"
+        >登录</a>
+        <span v-else class="my-2 my-lg-0">{{data.currentUser.name}}</span>
+      </nav>
     </div>
-    <router-view/>
+    <router-view></router-view>
+    <footer>
+      <p>
+        系统版本:{{data.systemInfo.version}}
+        <br>
+        启动时间: {{data.systemInfo.bootTime}}
+      </p>
+    </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+a {
+  color: -webkit-link;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+div > a:visited {
+  background: lightgray;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+div > a:link {
+  background: lightblue;
 }
 </style>
+<script>
+export default {
+  data: function() {
+    return {
+      data: {
+        currentUser: {},
+        systemInfo: {}
+      }
+    };
+  },
+  created: function() {
+    var that = this;
+    var query = `
+            {
+            currentUser{
+                name
+                email
+            }
+            systemInfo{
+                bootTime
+                version
+            }
+            }
+        `;
+    fetch("/api/graphql", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: query
+      })
+    })
+      .then(function(resp) {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          alert("请求失败:" + resp.status);
+        }
+      })
+      .then(function(resp) {
+        console.log(resp);
+        console.log(that.data);
+        if (resp && resp.data) {
+          console.log(resp.data);
+          that.data = resp.data;
+        }
+      });
+  }
+};
+</script>
+
